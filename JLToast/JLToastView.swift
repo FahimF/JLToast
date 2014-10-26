8b716a9a10ba2c44eed937d69e8b082f413d917f
Fahim Farook <fahimf@gmail.com>
2014-10-27 06:55:24 +0800

    Updated the license in preparation for adding breaking changes which deviate from the main source base - not a big fan of the language used :)

--- JLToast/JLToastView.swift
+++ JLToast/JLToastView.swift
@@ -1,19 +1,10 @@
 /*
  * JLToastView.swift
  *
- *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *                    Version 2, December 2004
+ * Original code by Su Yeol Jeon 2013-2014.
+ * Branched and modified by Fahim Farook 2014 to add additional functionality which did
+ * not meet with original author's requirements/specifications
  *
- * Copyright (C) 2013-2014 Su Yeol Jeon
- *
- * Everyone is permitted to copy and distribute verbatim or modified
- * copies of this license document, and changing it is allowed as long
- * as the name is changed.
- *
- *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
- *   TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
- *
- *  0. You just DO WHAT THE FUCK YOU WANT TO.
  *
  */
 
